35c2974f8a487a6e79989ef639456c53baca3bbe
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2020-12-25 20:31:09 +0900

    角を丸め

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let win = null;
-let subwin = null;
 
 app.on('ready', function () {
     win = new BrowserWindow({
@@ -21,50 +20,16 @@ app.on('ready', function () {
     win.setMenu(null);
     win.setTitle('Submarin');
     win.loadURL(`${__dirname}/index.html`)
-    // win.loadURL('https://submarin.online/');
 
     mainShortcuts();
 
     win.on('closed', function () {
         win = null
-        if (subwin) {
-            subwin.destroy();
-        }
         app.quit();
     });
 });
 
-function showSubWin() {
-    if (subwin) {
-        subwin.show();
-        subwin.focus();
-        return;
-    }
-
-    subwin = new BrowserWindow({
-        'icon': path.join(__dirname, '../icon.ico'),
-    });
-    subwin.setMenu(null);
-    subwin.setTitle('Submarin');
-    subwin.loadURL(__dirname + '/henkan.html');
-
-    subShortcuts();
-
-    subwin.on('close', (event) => {
-        event.preventDefault();
-        subwin.hide();
-    });
-
-    subwin.on('closed', function () {
-        subwin = null;
-    });
-};
-
 function mainShortcuts() {
-    localShortcut.register(win, 'CommandOrControl+E', function () {
-        showSubWin();
-    });
-
     localShortcut.register(win, 'CommandOrControl+Q', function () {
         app.quit();
     });
@@ -84,46 +49,4 @@ function mainShortcuts() {
     localShortcut.register(win, 'F12', function () {
         win.openDevTools();
     })
-
-    localShortcut.register(win, 'Alt+Left', function () {
-        win.webContents.goBack();
-    });
-
-    localShortcut.register(win, 'Alt+Right', function () {
-        win.webContents.goForward();
-    });
-
-    localShortcut.register(win, 'Alt+Home', function () {
-        win.loadURL('https://submarin.online/');
-    });
-};
-
-function subShortcuts() {
-    localShortcut.register(subwin, 'CommandOrControl+Q', function () {
-        subwin.hide();
-    });
-
-    localShortcut.register(subwin, ['CommandOrControl+R', 'F5'], function () {
-        subwin.reload();
-    });
-
-    localShortcut.register(subwin, 'F11', function () {
-        if (subwin.isFullScreen()) {
-            subwin.setSimpleFullScreen(false);
-        } else {
-            subwin.setSimpleFullScreen(true);
-        }
-    });
-
-    localShortcut.register(subwin, 'F12', function () {
-        subwin.openDevTools();
-    })
-
-    localShortcut.register(subwin, 'Alt+Left', function () {
-        subwin.webContents.goBack();
-    });
-
-    localShortcut.register(subwin, 'Alt+Right', function () {
-        subwin.webContents.goForward();
-    });
 };
