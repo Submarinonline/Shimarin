02ba38a3ffe16c695bd342732d402ef2271644a0
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-03-09 18:28:06 +0900

    copyするボタンを

--- src/index.js
+++ src/index.js
@@ -5,50 +5,62 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementsByClassName('titlebar-tab-active')[0].classList.remove('titlebar-tab-active');
             this.classList.add('titlebar-tab-active');
             document.getElementsByClassName('main-cont-show')[0].classList.remove('main-cont-show');
-            document.getElementById(`cont-${this.id}`).classList.add('main-cont-show');
+            getElement(`cont-${this.id}`).classList.add('main-cont-show');
         });
     }
 
-    document.getElementById('window-ctl-close').addEventListener('click', function () { window.api.close(); });
-    document.getElementById('window-ctl-restore').addEventListener('click', function () { window.api.restore(); });
-    document.getElementById('window-ctl-max').addEventListener('click', function () { window.api.max(); });
-    document.getElementById('window-ctl-min').addEventListener('click', function () { window.api.min(); });
+    getElement('window-ctl-close').addEventListener('click', function () { window.api.close(); });
+    getElement('window-ctl-restore').addEventListener('click', function () { window.api.restore(); });
+    getElement('window-ctl-max').addEventListener('click', function () { window.api.max(); });
+    getElement('window-ctl-min').addEventListener('click', function () { window.api.min(); });
 
-    document.getElementById('conv-select').addEventListener('change', function () {
-        document.getElementById('conv-frame').className = `selected-${this.value}`;
+    getElement('conv-select').addEventListener('change', function () {
+        getElement('conv-frame').className = `selected-${this.value}`;
+        gen();
     });
 
-    document.getElementById('conv-input').addEventListener('input', function () {
-        const ele = document.getElementById('conv-frame');
-        if (ele.classList.contains('selected-cjp')) {
-            window.api.genCjp(this.value).then(result => {
-                document.getElementById('conv-output').value = result;
-            });
-        } else if (ele.classList.contains('selected-mhr')) {
-            window.api.genMhr(this.value).then(result => {
-                document.getElementById('conv-output').value = result;
-            });
-        }
+    getElement('conv-copy').addEventListener('click', function () {
+        getElement('conv-output').select();
+        document.execCommand('copy');
     });
-    document.getElementById('conv-output').addEventListener('click', function () { this.select(); });
+
+    getElement('conv-clear').addEventListener('click', function () {
+        getElement('conv-input').value = '';
+        getElement('conv-output').value = '';
+    });
+
+    getElement('conv-input').addEventListener('input', function () { gen(); });
+    getElement('conv-output').addEventListener('click', function () { this.select(); });
 
     window.api.maximize(() => {
-        document.getElementById('window-ctl-max').classList.remove('window-ctl-show');
-        document.getElementById('window-ctl-restore').classList.add('window-ctl-show');
+        getElement('window-ctl-max').classList.remove('window-ctl-show');
+        getElement('window-ctl-restore').classList.add('window-ctl-show');
     });
 
     window.api.unmaximize(() => {
-        document.getElementById('window-ctl-max').classList.add('window-ctl-show');
-        document.getElementById('window-ctl-restore').classList.remove('window-ctl-show');
+        getElement('window-ctl-max').classList.add('window-ctl-show');
+        getElement('window-ctl-restore').classList.remove('window-ctl-show');
     });
 
-    window.api.enterFullScreen(() => {
-        document.body.classList.add('fullscreen');
-    });
+    window.api.enterFullScreen(() => { document.body.classList.add('fullscreen'); });
 
-    window.api.leaveFullScreen(() => {
-        document.body.classList.remove('fullscreen');
-    });
+    window.api.leaveFullScreen(() => { document.body.classList.remove('fullscreen'); });
 
     window.api.contentLoaded();
 });
+
+function getElement(id) { return document.getElementById(id); }
+
+function gen() {
+    const cf = getElement('conv-frame');
+    const ci = getElement('conv-input');
+    if (cf.classList.contains('selected-cjp')) {
+        window.api.genCjp(ci.value).then(result => {
+            getElement('conv-output').value = result;
+        });
+    } else if (cf.classList.contains('selected-mhr')) {
+        window.api.genMhr(ci.value).then(result => {
+            getElement('conv-output').value = result;
+        });
+    }
+}
