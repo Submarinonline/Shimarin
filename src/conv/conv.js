df100c6261a56c062ec6c5634f1fbaab82939925
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-06-18 20:04:29 +0900

    変更後

---
+++ src/conv/conv.js
@@ -0,0 +1,31 @@
+function gI(id) { return document.getElementById(id); }
+
+function gen() {
+    switch (gI('select').value) {
+        case 'cjp': {
+            window.parent.api.genCjp(gI('input').value).then(result => { gI('output').value = result; });
+            break;
+        }
+        case 'mhr': {
+            window.parent.api.genMhr(gI('input').value).then(result => { gI('output').value = result; });
+            break;
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    gI('select').addEventListener('change', function () { gen(); });
+
+    gI('copy').addEventListener('click', function () {
+        gI('output').select();
+        document.execCommand('copy');
+    });
+
+    gI('clear').addEventListener('click', function () {
+        gI('input').value = '';
+        gI('output').value = '';
+    });
+
+    gI('input').addEventListener('input', function () { gen(); });
+    gI('output').addEventListener('click', function () { this.select(); });
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Tab
-    for (const tab of gC('titlebar-tab')) {
+    for (const tab of gC('tab')) {
         tab.addEventListener('click', function () {
-            if (this.classList.contains('titlebar-tab-active')) return;
-            gC('titlebar-tab-active')[0].classList.remove('titlebar-tab-active');
-            this.classList.add('titlebar-tab-active');
+            if (this.classList.contains('tab-active')) return;
+            gC('tab-active')[0].classList.remove('tab-active');
+            this.classList.add('tab-active');
             gC('main-cont-show')[0].classList.remove('main-cont-show');
             gI(`cont-${this.id}`).classList.add('main-cont-show');
         });
@@ -28,23 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    // 変換
-    gI('conv-select').addEventListener('change', function () { gen(); });
-
-    gI('conv-copy').addEventListener('click', function () {
-        gI('conv-output').select();
-        document.execCommand('copy');
-    });
-
-    gI('conv-clear').addEventListener('click', function () {
-        gI('conv-input').value = '';
-        gI('conv-output').value = '';
-    });
-
-    gI('conv-input').addEventListener('input', function () { gen(); });
-    gI('conv-output').addEventListener('click', function () { this.select(); });
-
-
     // FullScreen
     window.api.enterFullScreen(() => { document.body.classList.add('fullscreen'); });
     window.api.leaveFullScreen(() => { document.body.classList.remove('fullscreen'); });
@@ -55,16 +38,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function gI(id) { return document.getElementById(id); }
 function gC(c) { return document.getElementsByClassName(c); }
-
-function gen() {
-    switch (gI('conv-select').value) {
-        case 'cjp': {
-            window.api.genCjp(gI('conv-input').value).then(result => { gI('conv-output').value = result; });
-            break;
-        }
-        case 'mhr': {
-            window.api.genMhr(gI('conv-input').value).then(result => { gI('conv-output').value = result; });
-            break;
-        }
-    }
-}
