835f30d22b4e3854cd7eb5028281f69fded53999
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-07-16 22:19:19 +0900

    task?

---
+++ tasks/package.js
@@ -0,0 +1,24 @@
+const packager = require('electron-packager');
+
+const opt = {
+    dir: '.',
+    out: 'out',
+    arch: 'x64',
+    platform: 'win32',
+    overwrite: true,
+    icon: 'icon/icon.ico',
+    ignore: '(.vscode|.gitignore|tasks|package-lock.json|.eslintrc.js)',
+};
+
+packager(opt);
+
+/*const linux = {
+    dir: '.',
+    out: 'out',
+    arch: 'x64',
+    platform: 'linux',
+    overwrite: true,
+    icon: 'icon/icon.png',
+    ignore: '(.vscode|.gitignore|tasks|package-lock.json|.eslintrc.js)',
+};*/
+//packager(linux);
