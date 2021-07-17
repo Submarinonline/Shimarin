ef99f0c0d830b2d3e9442e2c94796db1bd074bd8
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-07-17 10:11:35 +0900

    lintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,20 @@ module.exports = {
         'es2021': true,
         'node': true
     },
-    'extends': 'eslint:recommended',
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
     'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
         'ecmaVersion': 12,
         'sourceType': 'module'
     },
+    'plugins': [
+        'react'
+    ],
     'rules': {
         'indent': [
             'error',
