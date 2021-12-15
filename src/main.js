cbf5c58b3a4f9c9df3947aa59d6e7c2550737865
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-12-15 17:12:53 +0900

    c

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ const store = new Store();
 
 app.on('ready', () => {
     const win = new BrowserWindow({
-        width: store.get('window.width'),
-        height: store.get('window.height'),
+        width: store.get('window.width', dotProp.get(defaultConfig, 'window.width')),
+        height: store.get('window.height', dotProp.get(defaultConfig, 'window.height')),
         x: store.get('window.x'),
         y: store.get('window.y'),
         //minWidth: 535,
@@ -43,15 +43,15 @@ app.on('ready', () => {
         store.set('window.isMaximized', win.isMaximized());
     });
 
-    //win.on('maximize', () => win.webContents.send('window-maximize'));
-    //win.on('unmaximize', () => win.webContents.send('window-unmaximize'));
-    //win.on('enter-full-screen', () => win.webContents.send('enter-full-screen'));
-    //win.on('leave-full-screen', () => win.webContents.send('leave-full-screen'));
-
     //ipcMain.on('window-close', () => win.close());
     //ipcMain.on('window-unmaximize', () => win.unmaximize());
     //ipcMain.on('window-maximize', () => win.maximize());
     //ipcMain.on('window-minimize', () => win.minimize());
+
+    //win.on('maximize', () => win.webContents.send('window-maximize'));
+    //win.on('unmaximize', () => win.webContents.send('window-unmaximize'));
+    //win.on('enter-full-screen', () => win.webContents.send('enter-full-screen'));
+    //win.on('leave-full-screen', () => win.webContents.send('leave-full-screen'));
 
     ipcMain.on('set-config', (e, key, value) => store.set(key, value));
     ipcMain.on('reset-config', (e, key) => store.set(key, dotProp.get(defaultConfig, key)));
@@ -69,6 +69,7 @@ app.on('ready', () => {
         win.show();
     });
 
+    // register shortcuts
     for (const [key, func] of Object.entries(shortcuts)) {
         localShortcut.register(win, store.get(key, dotProp.get(defaultConfig, key)), func);
     }
