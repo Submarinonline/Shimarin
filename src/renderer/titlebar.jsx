3aa3db72a1505f7575b3ad2ac6eb42f3173c29b1
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-12-17 15:09:24 +0900

    classclassclassclassclass

--- src/renderer/titlebar.jsx
+++ src/renderer/titlebar.jsx
@@ -10,13 +10,94 @@ const windowCtlButton = theme => css`
     &:hover {
         background: ${theme.titleBar.ctlHover};
     }
+    & > svg {
+        fill: ${theme.titleBar.color};
+    }
 `;
 
-const windowCtlSVG = theme => css`
-    fill: ${theme.titleBar.color};
-`;
+const MinimizeBtn = () => {
+    return (
+        <div onClick={window.api.windowMinimize} css={[
+            windowCtlButton,
+            css`
+                grid-column: 1;
+            `
+        ]}>
+            <svg
+                width="10"
+                height="1"
+                viewBox="0 0 10 1"
+            >
+                <path d="M10 0v1H0V0z" />
+            </svg>
+        </div>
+    );
+};
 
-module.exports = class TitleBar extends React.Component {
+const UnmaximizeBtn = () => {
+    return (
+        <div onClick={window.api.windowUnmaximize} css={[
+            windowCtlButton,
+            css`
+                grid-column: 2;
+            `
+        ]}>
+            <svg
+                width="11"
+                height="11"
+                viewBox="0 0 11 11"
+            >
+                <path d="M11 9H9v2H0V2h2V0h9zM8 3H1v7h7zm2-2H3v1h6v6h1z" />
+            </svg>
+        </div>
+    );
+};
+
+const MaximizeBtn = () => {
+    return (
+        <div onClick={window.api.windowMaximize} css={[
+            windowCtlButton,
+            css`
+                grid-column: 2;
+            `
+        ]}>
+            <svg
+                width="10"
+                height="10"
+                viewBox="0 0 10 10"
+            >
+                <path d="M10 0v10H0V0zM9 1H1v8h8z" />
+            </svg>
+        </div>
+    );
+};
+
+const CloseBtn = () => {
+    return (
+        <div onClick={window.api.windowClose} css={[
+            windowCtlButton,
+            theme => css`
+                grid-column: 3;
+                &:hover {
+                    background: ${theme.titleBar.closeHover};
+                    & > svg {
+                        fill: ${theme.titleBar.closeHoverColor};
+                    }
+                }
+            `
+        ]}>
+            <svg
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+            >
+                <path d="M7 6l5 5-1 1-5-5-5 5-1-1 5-5-5-5 1-1 5 5 5-5 1 1z" />
+            </svg>
+        </div>
+    );
+};
+
+class WindowCtl extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -33,18 +114,24 @@ module.exports = class TitleBar extends React.Component {
             });
         });
     }
-    close() {
-        window.api.windowClose();
+    render() {
+        return (
+            <div css={css`
+                -webkit-app-region: no-drag;
+                display: grid;
+                grid-template-columns: 50px 50px 50px;
+                margin-left: auto;
+                height: 36px;
+            `}>
+                <MinimizeBtn />
+                {this.state.isMaximize ? <UnmaximizeBtn /> : <MaximizeBtn />}
+                <CloseBtn />
+            </div>
+        );
     }
-    maximize() {
-        window.api.windowMaximize();
-    }
-    unmaximize() {
-        window.api.windowUnmaximize();
-    }
-    minimize() {
-        window.api.windowMinimize();
-    }
+}
+
+module.exports = class TitleBar extends React.Component {
     render() {
         return (
             <div css={theme => css`
@@ -54,83 +141,7 @@ module.exports = class TitleBar extends React.Component {
                 display: flex;
                 background: ${theme.titleBar.bg};
             `}>
-                <div css={css`
-                    -webkit-app-region: no-drag;
-                    display: grid;
-                    grid-template-columns: 50px 50px 50px;
-                    margin-left: auto;
-                    height: 36px;
-                `}>
-                    <div onClick={this.minimize} css={[
-                        windowCtlButton,
-                        css`
-                            grid-column: 1;
-                        `
-                    ]}>
-                        <svg
-                            css={windowCtlSVG}
-                            width="10"
-                            height="1"
-                            viewBox="0 0 10 1"
-                        >
-                            <path d="M10 0v1H0V0z" />
-                        </svg>
-                    </div>
-                    {
-                        this.state.isMaximize ?
-                            <div onClick={this.unmaximize} css={[
-                                windowCtlButton,
-                                css`
-                                    grid-column: 2;
-                                `
-                            ]}>
-                                <svg
-                                    css={windowCtlSVG}
-                                    width="11"
-                                    height="11"
-                                    viewBox="0 0 11 11"
-                                >
-                                    <path d="M11 9H9v2H0V2h2V0h9zM8 3H1v7h7zm2-2H3v1h6v6h1z" />
-                                </svg>
-                            </div> :
-                            <div onClick={this.maximize} css={[
-                                windowCtlButton,
-                                css`
-                                    grid-column: 2;
-                                `
-                            ]}>
-                                <svg
-                                    css={windowCtlSVG}
-                                    width="10"
-                                    height="10"
-                                    viewBox="0 0 10 10"
-                                >
-                                    <path d="M10 0v10H0V0zM9 1H1v8h8z" />
-                                </svg>
-                            </div>
-                    }
-                    <div onClick={this.close} css={[
-                        windowCtlButton,
-                        theme => css`
-                            grid-column: 3;
-                            &:hover {
-                                background: ${theme.titleBar.closeHover};
-                                & > svg {
-                                    fill: ${theme.titleBar.closeHoverColor};
-                                }
-                            }
-                        `
-                    ]}>
-                        <svg
-                            css={windowCtlSVG}
-                            width="12"
-                            height="12"
-                            viewBox="0 0 12 12"
-                        >
-                            <path d="M7 6l5 5-1 1-5-5-5 5-1-1 5-5-5-5 1-1 5 5 5-5 1 1z" />
-                        </svg>
-                    </div>
-                </div>
+                <WindowCtl />
             </div>
         );
     }
